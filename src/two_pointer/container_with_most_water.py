fed381c5ba5a6a3dcf84aa03bd7993df17932ca6
nkhanhcong <nkhanhcong@gmail.com>
2024-06-04 21:43:51 +0700

    most contain water

--- src/two_pointer/container_with_most_water.py
+++ src/two_pointer/container_with_most_water.py
@@ -1,4 +1,17 @@
 
 def maxArea(height: list[int]) -> int:
+    left,right = 0, len(height)-1
+    max_area = 0
 
-    return 1
+    while (left != right):
+        current_area = min(height[left], height[right]) * (right- left)
+        max_area = max(max_area,current_area)
+        if height[left] >= height[right]:
+            right -=1
+        else:
+            left+=1
+
+
+    return max_area
+
+
